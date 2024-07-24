2210dd9d022a1fbab5eb84f50c392138b0bdb3d1
Go Away <baltzer.matt@gmail.com>
2024-07-23 18:47:20 -0700

    Created Player and Star classes to clean up the code and make it more readable

--- code/main.py
+++ code/main.py
@@ -2,6 +2,40 @@ import pygame
 from os.path import join
 from random import randint
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('images', 'player.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = (window_width / 2, window_height /2 + 100))
+        self.direction = pygame.math.Vector2()
+        self.speed = 300
+
+    def inputs(self):
+        # print(pygame.mouse.get_rel())
+        keys = pygame.key.get_pressed()
+        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt # This is the standard for moving something in pygame. rect.center += direction * speed * delta time
+
+        recent_keys = pygame.key.get_just_pressed()
+        if recent_keys[pygame.K_SPACE]:
+            print('fire laser')
+
+        # if player_rect.right >= window_width or player_rect.left <= 0:
+        #     player_direction.x *= -1
+        # if player_rect.top <= 0 or player_rect.bottom >= window_height:
+        #     player_direction.y *= -1
+
+    def update(self, dt):
+        self.inputs()
+        
+class Star(pygame.sprite.Sprite):
+    def __init__(self, groups, surf):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = (randint(0, window_width), randint(0, window_height)))
+
 # pygame setup
 pygame.init()
 window_width, window_height = 1280, 720
@@ -10,15 +44,12 @@ pygame.display.set_caption('Space shooter')
 clock = pygame.time.Clock()
 running = True
 
-# importing an image -> image becomes an surface when it is imported
-# imports :
-player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
-player_rect = player_surf.get_frect(center = (window_width / 2, window_height /2 + 100))
-player_direction = pygame.math.Vector2()
-player_speed = 300
-
+# groups
+all_sprites = pygame.sprite.Group()
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
-star_positions = [(randint(0, window_width), randint(0, window_height)) for i in range(20)]
+for i in range(20):
+    Star(all_sprites, star_surf)
+player = Player(all_sprites)
 
 meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
 meteor_rect = meteor_surf.get_frect(center = (window_width / 2, window_height / 2))
@@ -35,41 +66,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        # if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
-        #     print(1)
-        # if event.type == pygame.MOUSEMOTION:
-        #     player_rect.center = event.pos 
+    all_sprites.update(dt)
 
-    # inputs
-    # print(pygame.mouse.get_rel())
-    keys = pygame.key.get_pressed()
-    player_direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
-    player_direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
-    
-
-    player_rect.center += player_direction * player_speed * dt
-
-    # fill the screen with a color to wipe away anything from last frame
+    # draw the game
     display_surface.fill('black')
-    for pos in star_positions:
-        display_surface.blit(star_surf, pos)
-    
-
-    # if player_rect.right >= window_width or player_rect.left <= 0:
-    #     player_direction.x *= -1
-    # if player_rect.top <= 0 or player_rect.bottom >= window_height:
-    #     player_direction.y *= -1
-
-    display_surface.blit(meteor_surf, meteor_rect)
-    display_surface.blit(laser_surf, laser_rect)
-
-    # player movement
-    # This is the standard for moving something in pygame. rect.center += direction * speed * delta time
-    
-    display_surface.blit(player_surf,player_rect)
-    
-
-    # flip the display to put your work on the screen
-    pygame.display.update() # can also use pygame.display.flip() -> can update specific parts instead of the whole thing
+    all_sprites.draw(display_surface)    
+    pygame.display.update()
 
 pygame.quit()
