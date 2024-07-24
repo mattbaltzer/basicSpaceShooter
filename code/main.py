39cad609a7d162477e829637f491e5f69a367ee5
Go Away <baltzer.matt@gmail.com>
2024-07-23 17:43:07 -0700

    Added basic keyboard movement for the player

--- code/main.py
+++ code/main.py
@@ -10,38 +10,66 @@ pygame.display.set_caption('Space shooter')
 clock = pygame.time.Clock()
 running = True
 
-# basic surface
-surf = pygame.Surface((100,200))
-surf.fill('orange')
-x = 100
-
 # importing an image -> image becomes an surface when it is imported
+# imports :
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
-player_rect = player_surf.get_frect(center = (window_width / 2, window_height /2))
+player_rect = player_surf.get_frect(center = (window_width / 2, window_height /2 + 100))
+player_direction = pygame.math.Vector2()
+player_speed = 300
 
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, window_width), randint(0, window_height)) for i in range(20)]
 
+meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center = (window_width / 2, window_height / 2))
+
+laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (20, window_height - 20))
+
+
 while running:
+    dt = clock.tick() / 1000
     # event loop
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        # if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+        #     print(1)
+        # if event.type == pygame.MOUSEMOTION:
+        #     player_rect.center = event.pos 
+
+    # inputs
+    # print(pygame.mouse.get_rel())
+    keys = pygame.key.get_pressed()
+    player_direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+    player_direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+    
+
+    player_rect.center += player_direction * player_speed * dt
+
     # fill the screen with a color to wipe away anything from last frame
     display_surface.fill('black')
     for pos in star_positions:
         display_surface.blit(star_surf, pos)
-    if player_rect.right < window_width:
-        player_rect.left += 3
+    
+
+    # if player_rect.right >= window_width or player_rect.left <= 0:
+    #     player_direction.x *= -1
+    # if player_rect.top <= 0 or player_rect.bottom >= window_height:
+    #     player_direction.y *= -1
+
+    display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
+
+    # player movement
+    # This is the standard for moving something in pygame. rect.center += direction * speed * delta time
+    
     display_surface.blit(player_surf,player_rect)
+    
 
     # flip the display to put your work on the screen
     pygame.display.update() # can also use pygame.display.flip() -> can update specific parts instead of the whole thing
-
-    # limits the FPS to 60
-    clock.tick(60)  
-
 
 pygame.quit()
